e0314bfb4ac096ce8f064be77a97d43ca3e5078f
Cao Duc Anh <ducanh127nps@gmail.com>
2025-12-16 19:41:22 +0700

    Disable SSL in DB connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -15,7 +15,7 @@ const pool = new Pool({
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: false // Disable SSL as the database server doesn't support it
 });
 
 // Test database connection
